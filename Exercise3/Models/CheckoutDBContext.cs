91ba493fba8d5f44484591e736eaafd53c46c590
dell <dell@DESKTOP-SH2OB9S>
2021-03-19 01:54:05 +0800

    Add project files.

---
+++ Exercise3/Models/CheckoutDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exercise3.Models
+{
+    public class CheckoutDBContext : DbContext
+    {
+        public CheckoutDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ Exercise3/Pages/Customer.cshtml.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exercise3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Exercise3.Pages
+{
+    public class CustomerModel : PageModel
+    {
+        public CustomerModel(CheckoutDBContext checkcontext)
+        {
+            _checkOutdbcontext = checkcontext;
+        }
+        private readonly CheckoutDBContext _checkOutdbcontext;
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public List<Customer> Customers = new List<Customer>();
+
+        public void OnGet()
+        {
+            Customers = _checkOutdbcontext.Customers.ToList();
+        }
+        public ActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                Customers = _checkOutdbcontext.Customers.ToList();
+                return Page();
+            }
+            _checkOutdbcontext.Customers.Add(Customer);
+            _checkOutdbcontext.SaveChanges();
+            return Redirect("/customer");
+        }
+
+
+    }
+}
